8650078427c0c6c36a48d57e36dd94ee83e9bd61
泽鑫 蔡 <996209949@qq.com>
2022-07-23 03:12:03 +0800

    add IsActive property, and done serialization of project

--- FerrarisEditor/GameProject/Project.cs
+++ FerrarisEditor/GameProject/Project.cs
@@ -1,38 +1,88 @@
-﻿using System;
+﻿using FerrarisEditor.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace FerrarisEditor.GameProject
 {
-    [DataContract(Name ="Game")]
+    [DataContract(Name ="Game")] // xml name
     public class Project: ViewModelBase
     {
-        
+
         public static string Extension { get; } = ".ferraris";
 
         [DataMember]
-        public string Name { get; private set; }
+        public string Name { get; private set; } = "New Project";
         [DataMember]
         public string Path { get; private set; }
 
 
         public string FullPath => $"{Path}{Name}{Extension}";
-        [DataMember(Name = "Scenes")]
+        [DataMember(Name = "Scenes")] // xml name for scenes
         private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
-        public ReadOnlyCollection<Scene> Scenes
-        { get; }
+        public ReadOnlyObservableCollection<Scene> Scenes
+        { get; private set; }
+
+        private Scene _activeScene;
+
+        [DataMember]
+        public Scene ActiveScene
+        {
+            get => _activeScene;
+
+            set
+            {
+                if(_activeScene != value)
+                {
+                    _activeScene = value;
+                    OnPropertyChanged(nameof(ActiveScene));
+                }
+            }
+        }
+
+        public static Project Current => Application.Current.MainWindow.DataContext as Project;
+
+
+        // loading a project we just have a path, not project instance, so static method
+        public static Project Load(string file)
+        {
+            Debug.Assert(File.Exists(file));
+            return Serializer.FromFile<Project>(file);
+        }
+
+        public void Unload()
+        {
+
+        }
+
+        public static void Save(Project project)
+        {
+            Serializer.ToFile(project, project.FullPath);
+        }
+
+
+        [OnDeserialized]// call this function after serialized done
+        private void OnDeserialized(StreamingContext context)
+        {
+            if(_scenes!=null)
+            {
+                Scenes = new ReadOnlyObservableCollection<Scene>(_scenes);
+                OnPropertyChanged(nameof(Scenes));
+            }
+            ActiveScene = Scenes.FirstOrDefault(x => x.IsActive);
+        }
 
 
         public Project(string name, string path)
         {
             Name = name;
             Path = path;
-
-            _scenes.Add(new Scene(this, "Default scene"));
+            OnDeserialized(new StreamingContext());
         }
     }
 }
--- FerrarisEditor/GameProject/Scene.cs
+++ FerrarisEditor/GameProject/Scene.cs
@@ -11,7 +11,7 @@ namespace FerrarisEditor.GameProject
     [DataContract]
     public class Scene : ViewModelBase
     {
-        private string _name;
+        private string _name;// scene name
         [DataMember]
         public string Name
         {
@@ -27,8 +27,25 @@ namespace FerrarisEditor.GameProject
         }
 
         [DataMember]
-        public Project Project { get; private set; }
+        public Project Project { get; private set; } // easy to know which Project is ref to
 
+        // use Project get property of active
+        private bool _isActive;
+
+        [DataMember]
+        public bool IsActive
+        {
+            get => _isActive;
+
+            set
+            {
+                if(_isActive != value)
+                {
+                    _isActive = value;
+                    OnPropertyChanged(nameof(IsActive));
+                }
+            }
+        }
         public Scene(Project project, string name)
         {
             Debug.Assert(project != null);

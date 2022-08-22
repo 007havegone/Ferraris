6ba06049eb08b0ba5149ac61e9829ac2493ae917
泽鑫 蔡 <996209949@qq.com>
2022-08-22 16:29:56 +0800

    Add the abstract class MSComponent with interface for updating the MSComponent.

--- FerrarisEditor/Components/Component.cs
+++ FerrarisEditor/Components/Component.cs
@@ -13,6 +13,7 @@ namespace FerrarisEditor.Components
     [DataContract]
     abstract class Component:ViewModelBase
     {
+        public abstract IMSComponent GetMultiselectionComponent(MSEntity mSEntity);
         [DataMember]
         public GameEntity Owner { get; private set; }
 
@@ -22,9 +23,30 @@ namespace FerrarisEditor.Components
             Owner = owner;
         }
     }
-
+    // the basic MScomponent class for providing the interface.
     abstract class MSComponent<T> : ViewModelBase, IMSComponent where T : Component
     {
+        private bool _enableUpdates = true;// prevent the cyclic update
+        public List<T> SelectedComponents { get; }
 
+        protected abstract bool UpdateComponents(string propertyName);
+
+        protected abstract bool UpdateMSComponent();
+
+        public void Refresh()
+        {
+            _enableUpdates = false;
+            UpdateMSComponent();
+            _enableUpdates = true;
+        }
+
+        public MSComponent(MSEntity mSEntity)
+        {
+            Debug.Assert(mSEntity?.SelectedEntities?.Any() == true);
+            // get the component from the selected entity
+            SelectedComponents = mSEntity.SelectedEntities.Select(entity => entity.GetComponent<T>()).ToList();
+            // Update the components
+            PropertyChanged += (s, e) => { if (_enableUpdates) UpdateComponents(e.PropertyName); };
+        }
     }
 }

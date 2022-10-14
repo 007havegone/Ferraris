2988a4e6f30966f13f7a8cca6faa2212a6f5dae7
泽鑫 蔡 <996209949@qq.com>
2022-10-14 15:20:12 +0800

    Expose the EngineDLL API.

--- EngineDLL/EngineAPI.cpp
+++ EngineDLL/EngineAPI.cpp
@@ -1,2 +1,39 @@
 #include "Common.h"
+#include "CommonHeaders.h"
+
+/* following define remove the useless define include into this file,
+ * increase the compile speed.
+ */ 
+#ifndef WIN32_MEAN_AND_LEAN
+#define WIN32_MEAN_AND_LEAN
+#endif
+
+#include <Windows.h> // load dll by win32 function
+
+using namespace ferraris;
+
+namespace {
+	HMODULE game_code_dll{ nullptr };
+} // anonymous namespace
+
+EDITOR_INTERFACE u32
+LoadGameCodeDll(const char* dll_path)
+{
+	if (game_code_dll) return FALSE; // have loaded
+	game_code_dll = LoadLibraryA(dll_path);
+	assert(game_code_dll);
+
+	return game_code_dll ? TRUE : FALSE;// if load success
+}
+
+EDITOR_INTERFACE u32
+UnloadGameCodeDll()
+{
+	if (!game_code_dll) return FALSE;
+	assert(game_code_dll);
+	int result{ FreeLibrary(game_code_dll) };
+	assert(result);
+	game_code_dll = nullptr;
+	return TRUE;
+}
 

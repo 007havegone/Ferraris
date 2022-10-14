43c972ef37988396a51fed1d0b991dd1bf2cddca
泽鑫 蔡 <996209949@qq.com>
2022-10-14 15:20:53 +0800

    Expose the EngineDLL API to EngineEditor.

--- FerrarisEditor/DllWarpper/EngineAPI.cs
+++ FerrarisEditor/DllWarpper/EngineAPI.cs
@@ -24,33 +24,43 @@ namespace FerrarisEditor.DllWarpper
 {
     static class EngineAPI
     {
-        private const string _dllName = "EngineDll.dll";
+        private const string _engineDll = "EngineDll.dll";
 
-        [DllImport(_dllName)]
-        private static extern int CreateGameEntity(GameEntityDescriptor desc);
+        [DllImport(_engineDll, CharSet = CharSet.Ansi)]
+        public static extern int LoadGameCodeDll(string dllPath);
 
-        public static int CreateGameEntity(GameEntity entity)
+        [DllImport(_engineDll)]
+        public static extern int UnloadGameCodeDll();
+
+        internal static class EntityAPI
         {
-            GameEntityDescriptor desc = new GameEntityDescriptor();
+            [DllImport(_engineDll)]
+            private static extern int CreateGameEntity(GameEntityDescriptor desc);
 
-            // transform component
+            public static int CreateGameEntity(GameEntity entity)
             {
-                var c = entity.GetComponent<Transform>();
-                // copy to descriptor and use the dll function
-                desc.Transform.Position = c.Position;
-                desc.Transform.Rotation = c.Rotation;
-                desc.Transform.Scale = c.Scale;
+                GameEntityDescriptor desc = new GameEntityDescriptor();
+
+                // transform component
+                {
+                    var c = entity.GetComponent<Transform>();
+                    // copy to descriptor and use the dll function
+                    desc.Transform.Position = c.Position;
+                    desc.Transform.Rotation = c.Rotation;
+                    desc.Transform.Scale = c.Scale;
+                }
+                return CreateGameEntity(desc);
             }
-            return CreateGameEntity(desc);
-        }
 
-        [DllImport(_dllName)]
-        private static extern void RemoveGameEntity(int id);
+            [DllImport(_engineDll)]
+            private static extern void RemoveGameEntity(int id);
 
-        public static void RemoveGameEntity(GameEntity entity)
-        {
-            RemoveGameEntity(entity.EntityId);
+            public static void RemoveGameEntity(GameEntity entity)
+            {
+                RemoveGameEntity(entity.EntityId);
+            }
         }
+        
     }
 
 

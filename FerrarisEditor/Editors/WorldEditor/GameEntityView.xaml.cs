3c86168e929901816c5ae9bc819ea2494ba2a5df
泽鑫 蔡 <996209949@qq.com>
2022-10-16 22:37:18 +0800

    done the ScriptView used in GameEntityView, use the CompoentFactory to add new component.

--- FerrarisEditor/Editors/WorldEditor/GameEntityView.xaml.cs
+++ FerrarisEditor/Editors/WorldEditor/GameEntityView.xaml.cs
@@ -3,11 +3,13 @@ using FerrarisEditor.GameProject;
 using FerrarisEditor.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,6 +20,18 @@ using System.Windows.Shapes;
 
 namespace FerrarisEditor.Editors
 {
+    public class NullableBoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b && b == true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b && b == true;
+        }
+    }
     /// <summary>
     /// GameEntityView.xaml 的交互逻辑
     /// </summary>
@@ -93,5 +107,57 @@ namespace FerrarisEditor.Editors
             Project.UndoRedo.Add(new UndoRedoAction(undoAction, redoAction,
                 vm.IsEnabled == true ? "Enable game entity" : "Disable game entity"));
         }
+
+        private void OnAddComponent_Button_PreviewMouse_LBD(object sender, MouseButtonEventArgs e)
+        {
+            var menu = FindResource("addComponentMenu") as ContextMenu;
+            var btn = sender as ToggleButton;
+            btn.IsChecked = true;
+            menu.Placement = PlacementMode.Bottom;
+            menu.PlacementTarget = btn;
+            menu.MinWidth = btn.ActualWidth;
+            menu.IsOpen = true;
+        }
+
+        private void AddComponent(ComponentType componentType, object data)
+        {
+            var creationFunction = ComponentFactory.GetCreationFunction(componentType);
+            var changedEntities = new List<(GameEntity entity, Component component)>();
+            var vm = DataContext as MSEntity;
+            // record the changed entity for latter undo/redo
+            foreach (var entity in vm.SelectedEntities)
+            {
+                var component = creationFunction(entity, data);
+                if (entity.AddComponent(component))
+                {
+                    changedEntities.Add((entity, component));
+                }
+            }
+            if (changedEntities.Any())
+            {
+                vm.Refresh();
+
+                Project.UndoRedo.Add(new UndoRedoAction(
+                () =>
+                {
+                    changedEntities.ForEach(x => x.entity.RemoveComponent(x.component));
+                    (DataContext as MSEntity).Refresh();
+                },
+                () =>
+                {
+                    changedEntities.ForEach(x => x.entity.AddComponent(x.component));
+                    (DataContext as MSEntity).Refresh();
+                },
+                $"Add { componentType} component"));
+            }
+
+        }
+
+        private void OnAddScriptComponent(object sender, RoutedEventArgs e)
+        {
+            AddComponent(ComponentType.Script, (sender as MenuItem).Header.ToString());
+        }
+
+        
     }
 }

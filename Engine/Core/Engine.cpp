99ed30f02201924eb1b0e136e0ae30012017ab50
泽鑫 蔡 <996209949@qq.com>
2022-10-28 11:31:10 +0800

    done get game.bin and do update for the Script component.

--- Engine/Core/Engine.cpp
+++ Engine/Core/Engine.cpp
@@ -1,14 +1,24 @@
+
+#if !defined(SHIPPING)
+#include "..\Content\ContentLoader.h"
+#include "..\Components\Script.h"
+#include <thread>
+
 bool engine_initialize()
 {
-	return true;
+	bool result{ ferraris::content::load_game() };
+	return result;
 }
 void engine_update()
 {
-
+	ferraris::script::update(10.f);
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 void engine_shutdown()
 {
-
+	ferraris::content::unload_game();
 }
 
+#endif // !defined(SHIPPING)
 
+ 

28ecfa0feda02d8c710acd32977bc35d6f0eaf36
泽鑫 蔡 <996209949@qq.com>
2023-03-30 00:21:35 +0800

    Add a DelayEventTime for handling resize for the hosted window.

--- FerrarisEditor/Utilities/Utilities.cs
+++ FerrarisEditor/Utilities/Utilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace FerrarisEditor.Utilities
 {
@@ -23,6 +24,56 @@ namespace FerrarisEditor.Utilities
         {
             if (!value.HasValue || !other.HasValue) return false;
             return Math.Abs(value.Value - other.Value) < Epsilon;
+        }
+    }
+
+    class DelayEventTimerArgs : EventArgs
+    {
+        public bool RepeatEvent { get; set; }
+        public object Data { get; set; }
+        public DelayEventTimerArgs(object data)
+        {
+            Data = data;
+        }
+    }
+    class DelayEventTimer
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly TimeSpan _delay;
+        private DateTime _lastEventTime = DateTime.Now;
+        private object _data;
+
+        public event EventHandler<DelayEventTimerArgs> Triggered;
+
+        // this function call when the event is happen
+        public void Trigger(object data = null)
+        {
+            _data = data;
+            _lastEventTime = DateTime.Now;
+            _timer.IsEnabled = true;
+        }
+        public void Disable()
+        {
+            _timer.IsEnabled = false;
+        }
+        // once if the time interval greater then threshold,
+        // call the event handle function
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            if ((DateTime.Now - _lastEventTime) < _delay) return;
+            var eventArgs = new DelayEventTimerArgs(_data);
+            Triggered?.Invoke(this, eventArgs);
+            _timer.IsEnabled = eventArgs.RepeatEvent;
+        }
+
+        public DelayEventTimer(TimeSpan delay, DispatcherPriority priority = DispatcherPriority.Normal)
+        {
+            _delay = delay;
+            _timer = new DispatcherTimer(priority)
+            {
+                Interval = TimeSpan.FromMilliseconds(delay.TotalMilliseconds * 0.5)
+            };
+            _timer.Tick += OnTimerTick;
         }
     }
 }

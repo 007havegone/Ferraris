6f318d17f8593b46021bfe366786ad5c68910b86
泽鑫 蔡 <996209949@qq.com>
2023-06-23 12:15:28 +0800

    Update the position of the Windows.

--- EngineTest/TestRenderer.cpp
+++ EngineTest/TestRenderer.cpp
@@ -54,10 +54,10 @@ engine_test::initialize()
 	if (!realloc) return result;
 	platform::window_init_info info[]
 	{
-		{&win_proc, nullptr, L"Test window 1", 100 - 2000, 100, 400, 800},
-		{&win_proc, nullptr, L"Test window 2", 150 - 2000, 150, 800, 400},
-		{&win_proc, nullptr, L"Test window 3", 200 - 2000, 200, 400, 400},
-		{&win_proc, nullptr, L"Test window 4", 250 - 2000, 250, 800, 600},
+		{&win_proc, nullptr, L"Test window 1", 100, 100, 400, 800},
+		{&win_proc, nullptr, L"Test window 2", 150, 150, 800, 400},
+		{&win_proc, nullptr, L"Test window 3", 200, 200, 400, 400},
+		{&win_proc, nullptr, L"Test window 4", 250, 250, 800, 600},
 	};
 	static_assert(_countof(info) == _countof(_surfaces));
 

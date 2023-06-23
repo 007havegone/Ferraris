9d47ea5abdd96534cda4754c21572c2fb248734b
泽鑫 蔡 <996209949@qq.com>
2023-06-23 16:23:50 +0800

    Fixed the bug of the definition does not work.

--- Engine/Graphics/Direct3D12/D3D12Core.h
+++ Engine/Graphics/Direct3D12/D3D12Core.h
@@ -6,6 +6,10 @@ namespace ferraris::graphics::d3d12::core {
 bool initialize();
 void shutdown();
 void render();
+/// <summary>
+/// The general function for release the source in Direct 3D like Command Queue, List and adapter.
+/// </summary>
+/// <param name="resource"> The resource will release</param>
 template<typename T>
 constexpr void release(T*& resource)
 {
--- EngineTest/TestRenderer.cpp
+++ EngineTest/TestRenderer.cpp
@@ -1,9 +1,8 @@
-#ifdef TEST_RENDERER
 #include "..\Platform\PlatformTypes.h"
 #include "..\Platform\Platform.h"
 #include "..\Graphics\Renderer.h"
 #include "TestRenderer.h"
-
+#if TEST_RENDERER
 
 using namespace ferraris;
 // here we use the render_surface to abstract different Render API
@@ -71,6 +70,7 @@ void
 engine_test::run()
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	graphics::render();
 
 }
 void

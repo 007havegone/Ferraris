59b4eb435f0b245e2a1af395968fa08ce7fc031b
泽鑫 蔡 <996209949@qq.com>
2023-06-24 22:56:55 +0800

    Done support the Descriptor Heap and the deferred release for the resource.

--- Engine/Graphics/Direct3D12/D3D12Core.cpp
+++ Engine/Graphics/Direct3D12/D3D12Core.cpp
@@ -1,5 +1,5 @@
 #include "D3D12Core.h"
-
+#include "D3D12Resources.h"
 // Note that while ComPtr is used to manage the lifetime of resources on the CPU,
 // it has no understanding of the lifetime of resources on the GPU. Apps must account
 // for the GPU lifetime of resources to avoid destroying objects that may still be
@@ -134,6 +134,7 @@ private:
 		void release()
 		{
 			core::release(cmd_allocator);
+			fence_value = 0;
 		}
 		void wait(HANDLE fence_event, ID3D12Fence1* fence)
 		{
@@ -166,7 +167,14 @@ private:
 ID3D12Device8*					main_device{ nullptr };
 IDXGIFactory7*					dxgi_factory{ nullptr };
 d3d12_command					gfx_command;
+descriptor_heap					rtv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_RTV };
+descriptor_heap					dsv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_DSV };
+descriptor_heap					srv_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV };
+descriptor_heap					uav_desc_heap{ D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV };
 
+utl::vector<IUnknown*>			deferred_releases[frame_buffer_count]{};
+u32								deferred_release_flag[frame_buffer_count]{};
+std::mutex						deferred_release_mutex{};
 constexpr D3D_FEATURE_LEVEL minimum_feature_level{ D3D_FEATURE_LEVEL_11_0 };
 
 
@@ -218,7 +226,42 @@ get_max_feature_level(IDXGIAdapter4* adapter)
 	DXCall(device->CheckFeatureSupport(D3D12_FEATURE_FEATURE_LEVELS, &feature_level_info, sizeof(feature_level_info)));
 	return feature_level_info.MaxSupportedFeatureLevel;
 }
+
+void __declspec(noinline)
+process_deferred_releases(u32 frame_idx)
+{
+	std::lock_guard lock{ deferred_release_mutex };
+
+	// NOTE: we clear this flag in the beginning. If we'd clear it at the end.
+	//		 then it might overwrite some other thread that was trying to set it.
+	//		 It's fine if overwriting happens before processing the items.
+	deferred_release_flag[frame_idx] = false;
+	rtv_desc_heap.process_deferred_free(frame_idx);
+	dsv_desc_heap.process_deferred_free(frame_idx);
+	srv_desc_heap.process_deferred_free(frame_idx);
+	uav_desc_heap.process_deferred_free(frame_idx);
+	
+	utl::vector<IUnknown*>& resources{ deferred_releases[frame_idx] };
+	if (!resources.empty())
+	{
+		for (auto& resource : resources) release(resource);
+		resources.clear();
+	}
+
+}
 } // anonymous namespace
+
+namespace detail {
+
+void
+deferred_release(IUnknown* resource)
+{
+	const u32 frame_idx{ current_frame_index() };
+	std::lock_guard lock{ deferred_release_mutex };
+	deferred_releases[frame_idx].push_back(resource);
+	set_deferred_release_flag();
+}
+} // detail namespace
 
 bool
 initialize()
@@ -235,8 +278,15 @@ initialize()
 	// Using the bracket, the ComPtr will auto release
 	{
 		ComPtr<ID3D12Debug3> debug_interface;
-		DXCall(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_interface)));
-		debug_interface->EnableDebugLayer();
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_interface))))
+		{
+			debug_interface->EnableDebugLayer();
+		}
+		else
+		{
+			OutputDebugStringA("Warning: D3D12 Debug interface is not available, "
+				"Verify the Graphics Tools optional feature is installed in this device.");
+		}
 		dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
 	}
 
@@ -260,8 +310,6 @@ initialize()
 	DXCall(hr = D3D12CreateDevice(main_adapter.Get(), max_feature_level, IID_PPV_ARGS(&main_device)));
 	if (FAILED(hr)) return failed_init();
 
-	NAME_D3D12_OBJECT(main_device, L"Main D3D12 DEVICE");
-
 	// Here using the placement new to create the gfx_command
 	new(&gfx_command) d3d12_command(main_device, D3D12_COMMAND_LIST_TYPE_DIRECT);
 	if (!gfx_command.command_queue()) return failed_init();
@@ -275,7 +323,21 @@ initialize()
 		info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
 		info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
 	}
-#endif
+#endif // _DEBUG
+
+	bool result{ true };
+	result &= rtv_desc_heap.initialize(512, false);
+	result &= dsv_desc_heap.initialize(512, false);
+	result &= srv_desc_heap.initialize(4096, false);
+	result &= uav_desc_heap.initialize(512, false);
+
+	NAME_D3D12_OBJECT(main_device, L"Main D3D12 DEVICE");
+	NAME_D3D12_OBJECT(rtv_desc_heap.heap(), L"RTV Descriptor Heap");
+	NAME_D3D12_OBJECT(dsv_desc_heap.heap(), L"DSV Descriptor Heap");
+	NAME_D3D12_OBJECT(srv_desc_heap.heap(), L"SRV Descriptor Heap");
+	NAME_D3D12_OBJECT(uav_desc_heap.heap(), L"UAV Descriptor Heap");
+
+	if (!result) return failed_init();
 	return true;
 }
 
@@ -283,7 +345,25 @@ void
 shutdown()
 {
 	gfx_command.release();
+	// NOTE: we don't process_deferred_release at the end
+	//		 because some resources (such as swap chains) can't release before
+	//		 their depending resources are released.
+	for (u32 i{ 0 }; i < frame_buffer_count; ++i)
+	{
+		process_deferred_releases(i);
+	}
 	release(dxgi_factory);
+
+	rtv_desc_heap.release();
+	dsv_desc_heap.release();
+	srv_desc_heap.release();
+	uav_desc_heap.release();
+
+
+	// NOTE: some types only use deferred release for their resources during
+	//		 shutdown/reset/clear. To finnaly release these resources we call
+	//		 process_deferred_release once more.
+	process_deferred_releases(0);
 #ifdef _DEBUG
 	{
 		{
@@ -313,6 +393,11 @@ render()
 	gfx_command.begin_frame();
 	ID3D12GraphicsCommandList6* cmd_list{ gfx_command.command_list() };
 
+	const u32 frame_idx{ current_frame_index() };
+	if (deferred_release_flag[frame_idx])
+	{
+		process_deferred_releases(frame_idx);
+	}
 	// Record commands
 	// ...
 	// Done recording commands. Now execute commands,
@@ -321,9 +406,12 @@ render()
 }
 
 ID3D12Device* const 
-device()
-{
-	return main_device;
-}
+device() { return main_device; }
+
+u32 current_frame_index() { return gfx_command.frame_index(); }
+
+// followling is atom operator which does not need to lock.
+void
+set_deferred_release_flag() { deferred_release_flag[current_frame_index()] = 1; }
 }
 
--- Engine/Graphics/Direct3D12/D3D12Core.h
+++ Engine/Graphics/Direct3D12/D3D12Core.h
@@ -20,6 +20,21 @@ constexpr void release(T*& resource)
 	}
 }
 
-ID3D12Device* const device();
+namespace detail {
+void deferred_release(IUnknown* resource);
+}
 
+template<typename T>
+constexpr void deferred_release(T*& resource)
+{
+	if (resource)
+	{
+		detail::deferred_release(resource);
+		resource = nullptr;
+	}
+}
+
+ID3D12Device* const device();
+u32 current_frame_index();
+void set_deferred_release_flag();
 }
